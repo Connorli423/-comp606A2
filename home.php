be7a1fe4df1639f6430bc8bfb1c922bca8673d1c
liuxin96 <50728615+liuxin96@users.noreply.github.com>
2019-10-22 09:27:36 +1300

    OOP Version of Login, Registration, logout & sql

--- home.php
+++ home.php
@@ -1,54 +1,33 @@
-<?php 
-    session_start();
-?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<title>Home Page</title>
-<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<header>
-        <div class="logo">
-            <h1 class="logo-text"><span>Safe</span>Trade</h1> 
-        </div>
-        <i class="fa fa-bars menu-toggle"></i>
-            <ul class="nav">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <!-- <li><a href="#">SignUp</a></li>
-                <li><a href="#">Login</a></li> -->
-                <li>
-                    <a href="#">
-                        <i class="fa fa-user"></i>
-                         Register
-                        <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-                    </a>
-                    <ul>
-                        <li><a href="userRegister.php">Customer?</a></li>    
-                        <li><a href="trademanRegister.php">Trademan?</a></li> 
-                    </ul>
-                </li>
-            </ul>
-    </header>
-    <div class="wrap">
-		<h2>Register successfully</h2>
-		<h3>Welcome <?php echo $_SESSION["username"]; ?></h3>
-		<a href="login.php"><button type="button" class="back_btn"><< Ready to Login</button></a>
-    </div>
-</body>
-</html>
-
 <?php
-	if (isset($_SESSION['message'])) {
-		echo "<div id='error_msg'>".$_SESSION['message']."</div>";
-		unset($_SESSION['message']);
-	}
+
+session_start();
+include_once 'class_user.php';
+$user = new User(); $uid = $_SESSION['uid'];
+if (!$user->get_session()){
+ header("location:login.php");
+}
+
+if (isset($_GET['q'])){
+ $user->user_logout();
+ header("location:login.php");
+ }
 
 ?>
 
-</body>
-</html>
+
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<style><!--
+ body{
+ font-family:Arial, Helvetica, sans-serif;
+ }
+ h1{
+ font-family:'Georgia', Times New Roman, Times, serif;
+ }
+--></style>
+<div id="container">
+<div id="header"><a href="home.php?q=logout">LOGOUT</a></div>
+<div id="main-body">
+<h1>Hello <?php $user->get_fullname($uid); ?></h1>
+</div>
+<div id="footer"></div>
+</div>
--- login.php
+++ login.php
@@ -1,105 +1,65 @@
 <?php
-    session_start();
 
-    //connect to database
-    $con = mysqli_connect("localhost", "root", "", "loginSystem");
-    mysqli_query($con,"set names utf8");
-    if (!$con){
-       die("Could not connect:".mysqli_connect_error());
-    }
-	if (isset($_POST['login_btn'])) {
+session_start();
+	include_once 'class_user.php';
+	$user = new User();
 
-		$username = trim($_POST["username"]);
-		$password = trim($_POST["password"]);
-		
-		$password = md5($password);
-		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-		$result = mysqli_query($con, $sql);
-        $num = mysqli_num_rows($result);
-		if($num == 1) {
-			$_SESSION['message'] = "You are logged in now";
-			$_SESSION['username'] = $username;
-			header("location: home.php"); //redirect to home page
-		}else {
-			$_SESSION['message'] = "Username or password incorrect";
-		}
-	}
-?> 
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8"> 
-	<title>Welcome to login</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-
-<?php
-	if (isset($_SESSION['message'])) {
-		echo "<div id='error_msg'>".$_SESSION['message']."</div>";
-		unset($_SESSION['message']);
+	if (isset($_REQUEST['submit'])) {
+		extract($_REQUEST);
+	    $login = $user->check_login($emailusername, $password);
+	    if ($login) {
+	        // Registration Success
+	       header("location:home.php");
+	    } else {
+	        // Registration Failed
+	        echo 'Wrong username or password';
+	    }
 	}
 ?>
+	
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style><!--
+            #container{width:400px; margin: 0 auto;}
 
+--></style>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"> 
-    <title>loginSystem</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<header>
-        <div class="logo">
-            <h1 class="logo-text"><span>Safe</span>Trade</h1> 
-        </div>
-        <i class="fa fa-bars menu-toggle"></i>
-            <ul class="nav">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <!-- <li><a href="#">SignUp</a></li>
-                <li><a href="#">Login</a></li> -->
-                <li>
-                    <a href="#">
-                        <i class="fa fa-user"></i>
-                         Register
-                        <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-                    </a>
-                    <ul>
-                        <li><a href="userRegister.php">Customer?</a></li>    
-                        <li><a href="trademanRegister.php">Trademan?</a></li> 
-                    </ul>
-                </li>
-            </ul>
-    </header>
+<script type="text/javascript" language="javascript">
 
-<?php 
-    if(isset($_SESSSION['message'])) {
-        echo "<div id='error message'>" .$_SESSION['message']."</div>";
-        unset($_SESSION['message']);
-    }
+            function submitlogin() {
+                var form = document.login;
+				if(form.emailusername.value == ""){
+					alert( "Enter email or username." );
+					return false;
+				}
+				else if(form.password.value == ""){
+					alert( "Enter password." );
+					return false;
+				}
+			}
 
-?>
+</script>
 
-<div class="wrap">
-    <h2>Login</h2>      
-    <form method="post" action="login.php">
-            <tr>
-                <td>Username</td>
-                <td><input type="text" name="username" class="textInput" required></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><input type="password" name="password" class="textInput" required></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="login_btn" value="Login" required></td>
-            </tr>
-    </form>
-</div>
-</body>
-</html>
+<span style="font-family: 'Courier 10 Pitch', Courier, monospace; font-size: 13px; font-style: normal; line-height: 1.5;"><div id="container"></span>
+<h1>Login Here</h1>
+<form action="" method="post" name="login">
+<table>
+<tbody>
+<tr>
+<th>UserName or Email:</th>
+<td><input type="text" name="emailusername" required="" /></td>
+</tr>
+<tr>
+<th>Password:</th>
+<td><input type="password" name="password" required="" /></td>
+</tr>
+<tr>
+<td></td>
+<td><input onclick="return(submitlogin());" type="submit" name="submit" value="Login" /></td>
+</tr>
+<tr>
+<td></td>
+<td><a href="registration.php">Register new user</a></td>
+</tr>
+</tbody>
+</table>
+</form></div>
